528e99f564462937374cbd49d40966b245412e19
Pontus Tahir <pontus.tahir@gmail.com>
2021-01-07 13:57:23 +0100

    svg´s

--- index.js
+++ index.js
@@ -23,48 +23,48 @@ audio_file.onchange = function () {
     }
 
 
+    carBtn.addEventListener('click', () => {
+        const active = carBtn.getAttribute('data-active');
+        if (active === 'false') {
+            carBtn.setAttribute('data-active', 'true')
+            mobileBtn.setAttribute('data-active', 'false')
+            monoBtn.setAttribute('data-active', 'false')
+            body.style.backgroundColor = '#ff0000';
+        } else if (active === 'true') {
+            carBtn.setAttribute('data-active', 'false')
+            body.style.backgroundColor = 'white';
+        }
+    });
+    
+    
+    mobileBtn.addEventListener('click', () => {
+        const active = mobileBtn.getAttribute('data-active');
+        if (active === 'false') {
+            mobileBtn.setAttribute('data-active', 'true')
+            carBtn.setAttribute('data-active', 'false')
+            monoBtn.setAttribute('data-active', 'false')
+            body.style.backgroundColor = 'pink';
+        } else if (active === 'true') {
+            mobileBtn.setAttribute('data-active', 'false')
+            body.style.backgroundColor = 'white';
+        }
+    });
+    
+    
+    monoBtn.addEventListener('click', () => {
+        const active = monoBtn.getAttribute('data-active');
+        if (active === 'false') {
+            monoBtn.setAttribute('data-active', 'true')
+            mobileBtn.setAttribute('data-active', 'false')
+            carBtn.setAttribute('data-active', 'false')
+            console.log('mono')
+            body.style.backgroundColor = 'orange';
+        } else if (active === 'true') {
+            monoBtn.setAttribute('data-active', 'false')
+            body.style.backgroundColor = 'white';
+        }
+    });
 
 };
 
 
-carBtn.addEventListener('click', () => {
-    const active = carBtn.getAttribute('data-active');
-    if (active === 'false') {
-        carBtn.setAttribute('data-active', 'true')
-        mobileBtn.setAttribute('data-active', 'false')
-        monoBtn.setAttribute('data-active', 'false')
-        body.style.backgroundColor = '#ff0000';
-    } else if (active === 'true') {
-        carBtn.setAttribute('data-active', 'false')
-        body.style.backgroundColor = 'white';
-    }
-});
-
-
-mobileBtn.addEventListener('click', () => {
-    const active = mobileBtn.getAttribute('data-active');
-    if (active === 'false') {
-        mobileBtn.setAttribute('data-active', 'true')
-        carBtn.setAttribute('data-active', 'false')
-        monoBtn.setAttribute('data-active', 'false')
-        body.style.backgroundColor = 'pink';
-    } else if (active === 'true') {
-        mobileBtn.setAttribute('data-active', 'false')
-        body.style.backgroundColor = 'white';
-    }
-});
-
-
-monoBtn.addEventListener('click', () => {
-    const active = monoBtn.getAttribute('data-active');
-    if (active === 'false') {
-        monoBtn.setAttribute('data-active', 'true')
-        mobileBtn.setAttribute('data-active', 'false')
-        carBtn.setAttribute('data-active', 'false')
-        console.log('mono')
-        body.style.backgroundColor = 'orange';
-    } else if (active === 'true') {
-        monoBtn.setAttribute('data-active', 'false')
-        body.style.backgroundColor = 'white';
-    }
-});
