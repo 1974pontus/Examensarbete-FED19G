de03bc9f571c5ebe0e8545ca45a7d3792aa9a6e9
Pontus Tahir <pontus.tahir@gmail.com>
2021-01-11 10:39:16 +0100

    add class animate img

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const carBtn = document.querySelector('.carBtn');
 const mobileBtn = document.querySelector('.mobileBtn');
 const monoBtn = document.querySelector('.monoBtn');
-const carImg = document.querySelector('.carImg');
+const carImg = document.querySelector('#carImg');
 const mobileImg = document.querySelector('.mobileImg');
 const speakerImg = document.querySelector('.speakerImg');
 
@@ -27,9 +27,14 @@ audio_file.onchange = function () {
         const active = carBtn.getAttribute('data-active');
         if (active === 'false') {
             carImg.style.display = 'block';
+            if (carImg.style.display == 'block'){
+                carImg.classList.add('carAnimation')
+            }
             carBtn.setAttribute('data-active', 'true')
             mobileBtn.setAttribute('data-active', 'false')
             monoBtn.setAttribute('data-active', 'false')
+            mobileImg.style.display = 'none';
+            speakerImg.style.display = 'none';
         } else if (active === 'true') {
             carBtn.setAttribute('data-active', 'false')
             carImg.style.display = 'none';
@@ -41,9 +46,14 @@ audio_file.onchange = function () {
         const active = mobileBtn.getAttribute('data-active');
         if (active === 'false') {
             mobileImg.style.display = 'block';
+            if (mobileImg.style.display == 'block'){
+                mobileImg.classList.add('mobileAnimation')
+            }
             mobileBtn.setAttribute('data-active', 'true')
             carBtn.setAttribute('data-active', 'false')
             monoBtn.setAttribute('data-active', 'false')
+            carImg.style.display = 'none';
+            speakerImg.style.display = 'none';
         } else if (active === 'true') {
             mobileImg.style.display = 'none';
             mobileBtn.setAttribute('data-active', 'false')
@@ -55,10 +65,14 @@ audio_file.onchange = function () {
         const active = monoBtn.getAttribute('data-active');
         if (active === 'false') {
             speakerImg.style.display = 'block';
+            if (speakerImg.style.display == 'block'){
+                speakerImg.classList.add('speakerAnimation')
+            }
             monoBtn.setAttribute('data-active', 'true')
             mobileBtn.setAttribute('data-active', 'false')
             carBtn.setAttribute('data-active', 'false')
-            console.log('mono')
+            carImg.style.display = 'none';
+            mobileImg.style.display = 'none';
         } else if (active === 'true') {
             speakerImg.style.display = 'none';
             monoBtn.setAttribute('data-active', 'false')
